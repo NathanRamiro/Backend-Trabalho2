da7d99d070ef86f14358eff59fd30712e0cd0cf2
NathanRamiro <nathan.ramiro1912@gmail.com>
2021-05-31 13:24:38 -0300

    Novo campo

--- model/Aluno.js
+++ model/Aluno.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose')
 const AlunoSchema = mongoose.Schema({
     nome:String,
     dataMatricula:String,
+    curso:{
+        type:String,
+        enum:['ADS','Mecatronica','Eventos','Adm','Economia'],
+        default:'ads'
+    },
     cpf:{
         type:String,
         unique:true,
